67c6c34178cd519f2557e5f05e7503352352ffa0
Ivan Gonzalez Gonzalez <60527338+Ivan6801@users.noreply.github.com>
2022-07-06 19:24:59 -0700

    ADD pokeapi integration

--- src/components/Searcher/index.js
+++ src/components/Searcher/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { Grid, Search } from 'semantic-ui-react';
 import './styles.css';
 
 export default function SearchBar() {
   return (
     <div className='Searcher wrapper'>
-      <div>
-        <div
+      <Grid>
+        <Grid.Column
           widescreen={10}
           largeScreen={10}
           mobile={16}
           className='Searcher'
         >
-          <input
+          <Search
             aligned='right'
             input={{ fluid: true }}
             showNoResults={false}
             placeholder='Encuentra a tu Pokemón favorito'
           />
-        </div>
-      </div>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 }
